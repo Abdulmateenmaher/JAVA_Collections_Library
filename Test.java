d3c35ed150761a0e98d9b4602eea1b40f024ec49
Abdulmateen Maher <103168539+Abdulmateenmaher@users.noreply.github.com>
2022-11-03 13:31:28 +0430

    Added third file List

--- Test.java
+++ Test.java
@@ -1,24 +1,24 @@
-package LinkedList;
+package Lists;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        LinkedList ll=new LinkedList();
-        ll.add(55);
-        ll.add(44.3);
-        ll.add("maher");
-        ll.add('A');
-        System.out.println(ll);
+        List list=new LinkedList();
+        list.add("Car");
+        list.add("Bike");
+        list.add('A');
+        System.out.println(list);
+        list.remove("Car");
+        list.add(2022);
+        list.add(55.4);
+        System.out.println(list.get(3));
 
-        System.out.println( ll.removeFirst());
-         System.out.println(ll.removeLast());
-        System.out.println("this is first element now:"+ll.getFirst());
-        System.out.println(ll);
-        System.out.println();
-        System.out.println(ll.size());
-        System.out.println(ll.indexOf('A')+" must be deleted");
-        ll.clear();
-        System.out.println(ll+" must be empty");
+        list.add(4);
+        System.out.println(list.equals(4));
+        System.out.println(list);
+
+
     }
 }

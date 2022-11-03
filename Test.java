ffbe89f4f85f236200050ba31c8bb95bcbe30161
Abdulmateen Maher <103168539+Abdulmateenmaher@users.noreply.github.com>
2022-11-03 13:37:24 +0430

    added 6th file Stack LIFO vs Queue that is FiFo

--- Test.java
+++ Test.java
@@ -1,27 +1,31 @@
-package Queue;
+package Stack;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
+import javax.print.attribute.HashAttributeSet;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Stack;
 
 public class Test {
-    public static void main(String[] args) {
-        Queue q=new ArrayDeque();
-        //add element
-        q.add(55);
-        //also add element
-        q.offer("maher");
-        //peek the first element
-        System.out.println(q);
-        System.out.println(q.peek());
-        //remove the last element
-        q.remove();
-        System.out.println(q);
-        System.out.println(q.isEmpty());
+    public static void main(String[] args) throws Exception {
+        Stack s=new Stack();
+     s.add(55);
+     s.add(55.3);
+     s.add('A');
+     s.add("maher");
+        System.out.println(s.peek());
+        System.out.println(s.pop());
+        System.out.println(s.indexOf('A'));
 
-        q.add('A');
-        System.out.println(q);
-        q.clear();
-
+        s.empty();
+        s.push(45);
+        s.push("sabir");
+        System.out.println(s);
+        s.pop();
+        s.pop();
+        System.out.println(s);
+        s.empty();
+        System.out.println(s);
 
     }
+
 }

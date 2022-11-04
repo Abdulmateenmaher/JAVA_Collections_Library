76b05a6bca7194efcacc11d20e12d8cfdc8ce963
Abdulmateen Maher <103168539+Abdulmateenmaher@users.noreply.github.com>
2022-11-04 14:24:44 +0430

    uploaded 9 Hashset

--- Test.java
+++ Test.java
@@ -1,21 +1,30 @@
-package HashMap;
+package HashSet;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 
 public class Test {
     public static void main(String[] args) {
-        HashMap hm=new HashMap();
-        hm.put(4,"wasi");
-        hm.put(1,"maher");
-        
-        hm.put(0,"farid");
-        hm.put(5,"doublow");
-        hm.get(4);
-        hm.remove(4);
-        System.out.println(hm.entrySet());
-        System.out.println(hm.containsKey(5));
-
-        System.out.print(hm);
+        HashSet<String> HS=new HashSet<>();
+        HS.add("maher");
+        HS.add("Sabir");
+        HS.add("Qurishi");
+        HS.add("Mohammadi");
+        Iterator<String> Itr=HS.iterator();
+        while (Itr.hasNext()){
+            System.out.print(Itr.next()+" ");
+        }
+        HS.clone();
+        HS.size();
+        HashSet<String> hs=new HashSet<>();
+        hs.add("Sidiqi");
+        hs.add("wasi");
+        hs.add("afghan");
+        HS.addAll(hs);
+        System.out.println();
+        Iterator<String> itr=HS.iterator();
+        while (itr.hasNext()){
+            System.out.print(itr.next()+" ");
+        }
     }
 }

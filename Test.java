2849dfed1fb3b51caadeffbe631a26357dae9bfd
Abdulmateen Maher <103168539+Abdulmateenmaher@users.noreply.github.com>
2022-11-04 14:25:27 +0430

    uploaded 10 Linked_HashSet

--- Test.java
+++ Test.java
@@ -1,30 +1,26 @@
-package HashSet;
+package Linked_Hashset;
 
-import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 
 public class Test {
     public static void main(String[] args) {
-        HashSet<String> HS=new HashSet<>();
-        HS.add("maher");
-        HS.add("Sabir");
-        HS.add("Qurishi");
-        HS.add("Mohammadi");
-        Iterator<String> Itr=HS.iterator();
-        while (Itr.hasNext()){
-            System.out.print(Itr.next()+" ");
-        }
-        HS.clone();
-        HS.size();
-        HashSet<String> hs=new HashSet<>();
-        hs.add("Sidiqi");
-        hs.add("wasi");
-        hs.add("afghan");
-        HS.addAll(hs);
-        System.out.println();
-        Iterator<String> itr=HS.iterator();
+        LinkedHashSet lhs=new LinkedHashSet();
+        lhs.add("maher");
+        lhs.add("wasi");
+        lhs.add("malik");
+        LinkedHashSet Lhs=new LinkedHashSet();
+        Lhs.add("saboor");
+        Lhs.add("Danish");
+        lhs.addAll(Lhs);
+        Iterator<String>itr=lhs.iterator();
         while (itr.hasNext()){
-            System.out.print(itr.next()+" ");
+            System.out.print(itr.next()+",");
         }
+        lhs.remove("malik");
+        lhs.isEmpty();
+        lhs.equals("dawood");
+
     }
 }
